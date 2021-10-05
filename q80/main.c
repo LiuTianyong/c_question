c450ec1293542bca76838527497e1121860095c6
刘天勇 <1063331689@qq.com>
2021-10-05 16:47:44 +0800

    文件读写

--- q80/main.c
+++ q80/main.c
@@ -1,6 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char file_name[20] = "../a.txt";
+    FILE *fpw = fopen(file_name, "w");
+
+    char str[100] = "abcdefghigklmnopqrstuvwxyz";
+    if (NULL == fpw) {
+        printf("´ò¿ªÊ§°Ü\n");
+        exit(0);
+    }
+
+    fputs(str,fpw);
+
+    fclose(fpw);
+
+
+    FILE *fpr = fopen(file_name,"r");
+    char res[100];
+
+    fgets(res,5,fpr);
+    printf("%s",res);
+    fclose(fpr);
+
     return 0;
 }
